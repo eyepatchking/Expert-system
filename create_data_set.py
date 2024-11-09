f9c1f0d27e4c8465d0d80a68041a5a06d56324ac
Dilyorbek <eyepatchkin9@gmail.com>
2024-11-09 17:03:31 +0500

    Update create_data_set.py

--- create_data_set.py
+++ create_data_set.py
@@ -1,4 +1,5 @@
-import pandas as pd
+import random
+import csv
 
 data = {
     'Yosh': [25, 45, 35, 50, 40, 60, 30, 55, 42, 38],
@@ -10,6 +11,39 @@ data = {
     'Target': [0, 1, 0, 1, 0, 1, 0, 1, 0, 0]
 }
 
-df = pd.DataFrame(data)
-df.to_csv('malumotlar.csv', index=False)
+
+for _ in range(90000):
+    yosh = random.randint(20, 70)
+    vazn = random.randint(50, 120)
+    qon_bosim = random.randint(110, 180)
+    diastolik_qon_bosim = random.randint(70, 100)
+    xolesterin = random.randint(150, 250)
+    glukoza = random.randint(80, 150)
+
+    
+    if yosh > 50 and qon_bosim > 140:  
+        target = 1
+    elif xolesterin > 240 or glukoza > 120: 
+        target = 1
+    else:
+        target = 0  
+
+    data['Yosh'].append(yosh)
+    data['Vazn'].append(vazn)
+    data['Qon_Bosim'].append(qon_bosim)
+    data['Diastolik_Qon_Bosim'].append(diastolik_qon_bosim)
+    data['Xolesterin'].append(xolesterin)
+    data['Glukoza'].append(glukoza)
+    data['Target'].append(target)
+
+
+with open('data.csv', mode='w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=data.keys())
+    writer.writeheader()
+
+   
+    for i in range(len(data['Yosh'])):
+        row = {key: data[key][i] for key in data}
+        writer.writerow(row)
+
 print("Ma'lumotlar faylga saqlandi.")
